c33b882bb6cdf9c3f863728c9ac3f1704e4e3c2e
Pa-fs <qordi124@gmail.com>
2024-07-29 09:27:40 +0900

    Added dynamic menu changed

--- js/category.js
+++ js/category.js
@@ -1,12 +1,33 @@
-let categoryMenus = document.querySelectorAll(".menu_category_list a");
+import * as product from "./product.js";
+
+let categoryMenusList = document.querySelectorAll(".menu_category_list");
 
 function focusAfterClick(event) {
-  for (let i = 0; i < categoryMenus.length; i++) {
-    categoryMenus[i].classList.remove("on");
+  // 메뉴 어느 곳이든 클릭했을 때 항상 같은 결과를 내도록 함
+
+  // 모든 메뉴 항목에서 on 클래스 제거
+  for (let i = 0; i < categoryMenusList.length; i++) {
+    categoryMenusList[i].querySelector("a").classList.remove("on");
   }
-  event.target.classList.add("on");
+
+  // 이벤트 타겟에 따라 가장 가까운 해당 부모 요소 찾음
+  let targetMenu = event.target.closest(".menu_category_list");
+
+  // 부모 요소 찾고 자식 a태그에 on 클래스 추가
+  if (targetMenu) {
+    targetMenu.querySelector("a").classList.add("on");
+  }
+
+  // rendering
+  const categoryId = targetMenu.getAttribute("data-category");
+
+  product.renderRecommendedProducts(categoryId);
+  product.renderProducts(categoryId);
 }
 
-for (let i = 0; i < categoryMenus.length; i++) {
-  categoryMenus[i].onclick = focusAfterClick;
+for (let i = 0; i < categoryMenusList.length; i++) {
+  categoryMenusList[i].onclick = focusAfterClick;
 }
+
+product.renderRecommendedProducts(1);
+product.renderProducts(1);
---
+++ js/product.js
@@ -0,0 +1,555 @@
+const productsData = {
+  1: [
+    {
+      name: "소금빵",
+      image: "../img/bread/1.jpg",
+      description: "짜지 않는 고소함",
+      kind: "Bread",
+    },
+    {
+      name: "소금빵",
+      image: "../img/bread/2.jpg",
+      description: "짜지 않는 고소함",
+      kind: "Bread",
+    },
+    {
+      name: "소금빵",
+      image: "../img/bread/3.jpg",
+      description: "짜지 않는 고소함",
+      kind: "Bread",
+    },
+    {
+      name: "소금빵",
+      image: "../img/bread/4.jpg",
+      description: "짜지 않는 고소함",
+      kind: "Bread",
+    },
+    {
+      name: "소금빵",
+      image: "../img/bread/5.jpg",
+      description: "짜지 않는 고소함",
+      kind: "Bread",
+    },
+    {
+      name: "소금빵",
+      image: "../img/bread/6.jpg",
+      description: "짜지 않는 고소함",
+      kind: "Bread",
+    },
+    {
+      name: "소금빵",
+      image: "../img/bread/7.jpg",
+      description: "짜지 않는 고소함",
+      kind: "Bread",
+    },
+    {
+      name: "소금빵",
+      image: "../img/bread/8.jpg",
+      description: "짜지 않는 고소함",
+      kind: "Bread",
+    },
+    {
+      name: "소금빵",
+      image: "../img/bread/9.jpg",
+      description: "짜지 않는 고소함",
+      kind: "Bread",
+    },
+    {
+      name: "소금빵",
+      image: "../img/bread/10.jpg",
+      description: "짜지 않는 고소함",
+      kind: "Bread",
+    },
+  ],
+  2: [
+    {
+      name: "마담 얼그레이 케이크",
+      image: "../img/cake/1.jpg",
+      description: "짜지 않는 고소함",
+      kind: "Cake",
+      new: "item_new"
+    },
+    {
+      name: "말차에 퐁당 딸기케이크",
+      image: "../img/cake/2.jpg",
+      description: "짜지 않는 고소함",
+      kind: "Cake",
+    },
+    {
+      name: "사르르 초코 생크림",
+      image: "../img/cake/3.jpg",
+      description: "짜지 않는 고소함",
+      kind: "Cake",
+    },
+    {
+      name: "부드러운 고구마 라떼 케이크",
+      image: "../img/cake/4.jpg",
+      description: "짜지 않는 고소함",
+      kind: "Cake",
+    },
+    {
+      name: "우유 퐁당 바닐라",
+      image: "../img/cake/5.jpg",
+      description: "짜지 않는 고소함",
+      kind: "cake",
+    },
+    {
+      name: "스트로베리 쇼콜라",
+      image: "../img/cake/6.jpg",
+      description: "짜지 않는 고소함",
+      kind: "Cake",
+    },
+    {
+      name: "티라미수 생크림",
+      image: "../img/cake/7.jpg",
+      description: "짜지 않는 고소함",
+      kind: "Cake",
+    },
+    {
+      name: "해피 복숭아 케이크",
+      image: "../img/cake/8.jpg",
+      description: "짜지 않는 고소함",
+      kind: "Cake",
+    },
+    {
+      name: "사르르 샤사핑",
+      image: "../img/cake/9.jpg",
+      description: "짜지 않는 고소함",
+      kind: "Cake",
+    },
+    {
+      name: "올스타 케이크",
+      image: "../img/cake/10.jpg",
+      description: "짜지 않는 고소함",
+      kind: "Cake",
+    },
+  ],
+  3: [
+    {
+      name: "소금빵",
+      image: "../img/dessert/1.jpg",
+      description: "짜지 않는 고소함",
+      kind: "Dessert",
+    },
+    {
+      name: "소금빵",
+      image: "../img/dessert/2.jpg",
+      description: "짜지 않는 고소함",
+      kind: "Dessert",
+    },
+    {
+      name: "소금빵",
+      image: "../img/dessert/3.jpg",
+      description: "짜지 않는 고소함",
+      kind: "Dessert",
+    },
+    {
+      name: "소금빵",
+      image: "../img/dessert/4.jpg",
+      description: "짜지 않는 고소함",
+      kind: "Dessert",
+    },
+    {
+      name: "소금빵",
+      image: "../img/Dessert/5.jpg",
+      description: "짜지 않는 고소함",
+      kind: "Dessert",
+    },
+    {
+      name: "소금빵",
+      image: "../img/dessert/6.jpg",
+      description: "짜지 않는 고소함",
+      kind: "Dessert",
+    },
+    {
+      name: "소금빵",
+      image: "../img/dessert/7.jpg",
+      description: "짜지 않는 고소함",
+      kind: "Dessert",
+    },
+    {
+      name: "소금빵",
+      image: "../img/dessert/8.jpg",
+      description: "짜지 않는 고소함",
+      kind: "Dessert",
+    },
+    {
+      name: "소금빵",
+      image: "../img/dessert/9.jpg",
+      description: "짜지 않는 고소함",
+      kind: "Dessert",
+    },
+    {
+      name: "소금빵",
+      image: "../img/dessert/10.jpg",
+      description: "짜지 않는 고소함",
+      kind: "Dessert",
+    },
+  ],
+  4: [
+    {
+      name: "소금빵",
+      image: "../img/sandwich/1.jpg",
+      description: "짜지 않는 고소함",
+      kind: "Sandwich",
+    },
+    {
+      name: "소금빵",
+      image: "../img/sandwich/2.jpg",
+      description: "짜지 않는 고소함",
+      kind: "Sandwich",
+    },
+    {
+      name: "소금빵",
+      image: "../img/sandwich/3.jpg",
+      description: "짜지 않는 고소함",
+      kind: "Sandwich",
+    },
+    {
+      name: "소금빵",
+      image: "../img/sandwich/4.jpg",
+      description: "짜지 않는 고소함",
+      kind: "Sandwich",
+    },
+    {
+      name: "소금빵",
+      image: "../img/sandwich/5.jpg",
+      description: "짜지 않는 고소함",
+      kind: "Sandwich",
+    },
+    {
+      name: "소금빵",
+      image: "../img/sandwich/6.jpg",
+      description: "짜지 않는 고소함",
+      kind: "Sandwich",
+    },
+    {
+      name: "소금빵",
+      image: "../img/sandwich/7.jpg",
+      description: "짜지 않는 고소함",
+      kind: "Sandwich",
+    },
+    {
+      name: "소금빵",
+      image: "../img/sandwich/8.jpg",
+      description: "짜지 않는 고소함",
+      kind: "Sandwich",
+    },
+    {
+      name: "소금빵",
+      image: "../img/sandwich/9.jpg",
+      description: "짜지 않는 고소함",
+      kind: "Sandwich",
+    },
+    {
+      name: "소금빵",
+      image: "../img/sandwich/10.jpg",
+      description: "짜지 않는 고소함",
+      kind: "Sandwich",
+    },
+  ],
+};
+
+const recommendedProductsLeftData = {
+  1: [
+    {
+      name: "소금빵",
+      image: "../img/bread/10.jpg",
+      description: "짜지 않는 고소함",
+      kind: "Bread",
+    },
+  ],
+  2: [
+    {
+      name: "말차에 퐁당 딸기케이크",
+      image: "../img/cake/2.jpg",
+      description: "짜지 않는 고소함",
+      kind: "Cake",
+    },
+  ],
+  3: [
+    {
+      name: "쿠키생크림 조합",
+      image: "../img/dessert/1.jpg",
+      description: "짜지 않는 고소함",
+      kind: "Dessert",
+    },
+  ],
+  4: [
+    {
+      name: "햄치즈 샌드위치",
+      image: "../img/sandwich/10.jpg",
+      description: "짜지 않는 고소함",
+      kind: "Sandwich",
+    },
+  ],
+};
+
+const recommendedProductsRightData = {
+  1: [
+    {
+      name: "티라미수 생크림",
+      image: "../img/bread/7.jpg",
+      description: "짜지 않는 고소함",
+      kind: "Bread",
+    },
+    {
+      name: "해피 복숭아 케이크",
+      image: "../img/bread/8.jpg",
+      description: "짜지 않는 고소함",
+      kind: "Bread",
+    },
+    {
+      name: "사르르 샤사핑",
+      image: "../img/bread/9.jpg",
+      description: "짜지 않는 고소함",
+      kind: "Bread",
+    },
+    {
+      name: "올스타 케이크",
+      image: "../img/bread/10.jpg",
+      description: "짜지 않는 고소함",
+      kind: "Bread",
+    },
+  ],
+  2: [
+    {
+      name: "티라미수 생크림",
+      image: "../img/cake/7.jpg",
+      description: "짜지 않는 고소함",
+      kind: "Cake",
+    },
+    {
+      name: "해피 복숭아 케이크",
+      image: "../img/cake/8.jpg",
+      description: "짜지 않는 고소함",
+      kind: "Cake",
+    },
+    {
+      name: "사르르 샤사핑",
+      image: "../img/cake/9.jpg",
+      description: "짜지 않는 고소함",
+      kind: "Cake",
+    },
+    {
+      name: "올스타 케이크",
+      image: "../img/cake/10.jpg",
+      description: "짜지 않는 고소함",
+      kind: "Cake",
+    },
+  ],
+  3: [
+    {
+      name: "티라미수 생크림",
+      image: "../img/dessert/7.jpg",
+      description: "짜지 않는 고소함",
+      kind: "Dessert",
+    },
+    {
+      name: "해피 복숭아 케이크",
+      image: "../img/dessert/8.jpg",
+      description: "짜지 않는 고소함",
+      kind: "Dessert",
+    },
+    {
+      name: "사르르 샤사핑",
+      image: "../img/dessert/9.jpg",
+      description: "짜지 않는 고소함",
+      kind: "Dessert",
+    },
+    {
+      name: "올스타 케이크",
+      image: "../img/dessert/10.jpg",
+      description: "짜지 않는 고소함",
+      kind: "Dessert",
+    },
+  ],
+  4: [
+    {
+      name: "티라미수 생크림",
+      image: "../img/sandwich/7.jpg",
+      description: "짜지 않는 고소함",
+      kind: "Sandwich",
+    },
+    {
+      name: "해피 복숭아 케이크",
+      image: "../img/sandwich/8.jpg",
+      description: "짜지 않는 고소함",
+      kind: "Sandwich",
+    },
+    {
+      name: "사르르 샤사핑",
+      image: "../img/sandwich/9.jpg",
+      description: "짜지 않는 고소함",
+      kind: "Sandwich",
+    },
+    {
+      name: "올스타 케이크",
+      image: "../img/sandwich/10.jpg",
+      description: "짜지 않는 고소함",
+      kind: "Sandwich",
+    },
+  ],
+};
+
+let recommendedProductLeftSideRender = document.querySelector(
+  ".recommended_left_area"
+);
+let recommendedProductRightSideRender = document.querySelector(
+  ".recommended_right_area"
+);
+let productRender = document.querySelector(".product_rendering");
+
+export function renderRecommendedProducts(categoryId) {
+  /* common structure
+    li              .recommended_item_wrap
+    -- a    
+    ---- span       .img
+    ------ img          src
+    ---- span       .info
+    ------ span         .name
+    ------ span         .txt_shape
+   */
+  eraseProducts(recommendedProductLeftSideRender);
+  eraseProducts(recommendedProductRightSideRender);
+
+  /* left side */
+  drawRecommendedProducts(
+    recommendedProductLeftSideRender,
+    recommendedProductsLeftData,
+    categoryId
+  );
+
+  /* right side */
+  drawRecommendedProducts(
+    recommendedProductRightSideRender,
+    recommendedProductsRightData,
+    categoryId
+  );
+}
+
+export function renderProducts(categoryId) {
+  // 기존 컨텐츠 지우기
+  eraseProducts(productRender);
+
+  const products = productsData[categoryId];
+
+  products.forEach((product) => {
+    /*
+        product DOM structure
+
+        li
+        -- a        .item_best
+        ---- span       .item_text
+        ---- img        .item_img 
+        ---- span       .item_info
+        ------ span         .name
+        ------ span         .txt_shape
+        ------ span         .item_new
+    */
+    const productItem = document.createElement("li");
+    productItem.className = "item";
+
+    const productAItem = document.createElement("a");
+
+    const productName = document.createElement("span");
+    productName.className = "item_text";
+    productName.textContent = product.name;
+    productItem.appendChild(productName);
+
+    const productImage = document.createElement("img");
+    productImage.className = "item_img";
+    productImage.src = product.image;
+    productItem.appendChild(productImage);
+
+    const productItemInfo = document.createElement("span");
+    productItemInfo.className = "item_info";
+    const productDescInfo = document.createElement("span");
+    productDescInfo.className = "name";
+    productDescInfo.textContent = product.description;
+    const productKindInfo = document.createElement("span");
+    productKindInfo.className = "txt_shape";
+    productKindInfo.textContent = product.kind;
+
+    productItemInfo.appendChild(productDescInfo);
+    productItemInfo.appendChild(productKindInfo);
+
+    if(product.new) {
+      const productNew = document.createElement("span");
+      productNew.className = product.new;
+      productItemInfo.appendChild(productNew);
+    }
+
+    productItem.appendChild(productItemInfo);
+
+    productAItem.appendChild(productName);
+    productAItem.appendChild(productImage);
+    productAItem.appendChild(productItemInfo);
+    productItem.appendChild(productAItem);
+    productRender.appendChild(productItem);
+  });
+
+  // Longer text slice
+  const itemTexts = document.querySelectorAll(".product_rendering .item_text");
+  itemTexts.forEach((itemText) => doTextSlice(itemText, 160));
+
+  // box-shadow changed
+  const itemList = document.querySelectorAll(".product_rendering .item a");
+  itemList.forEach((item) => {
+    item.style.boxShadow = `2px 2px 8px 2px rgba(${255 - categoryId * 20}, ${
+      180 - categoryId * 20
+    }, ${41 + categoryId * 20}, 0.9)`;
+  });
+}
+
+function doTextSlice(element, maxWidth) {
+  let text = element.innerText;
+
+  // offsetWidth = width + padding + margin;
+  while (element.offsetWidth > maxWidth) {
+    text = text.slice(0, -1);
+    element.innerText = text + "..";
+  }
+}
+
+function drawRecommendedProducts(renderProduct, productsData, categoryId) {
+  const products = productsData[categoryId];
+
+  products.forEach((product) => {
+    const itemWrap = document.createElement("li");
+    itemWrap.className = "recommended_item_wrap";
+
+    const itemWrapATag = document.createElement("a");
+    const itemSpanImg = document.createElement("span");
+    itemSpanImg.className = "img";
+    const itemImg = document.createElement("img");
+    itemImg.src = product.image;
+
+    const itemInfo = document.createElement("span");
+    itemInfo.className = "info";
+    const itemInfoName = document.createElement("span");
+    itemInfoName.className = "name";
+    itemInfoName.textContent = product.name;
+    const itemInfoKind = document.createElement("span");
+    itemInfoKind.className = "txt_shape";
+    itemInfoKind.textContent = product.kind;
+
+    itemInfo.appendChild(itemInfoName);
+    itemInfo.appendChild(itemInfoKind);
+
+    itemSpanImg.appendChild(itemImg);
+
+    itemWrapATag.appendChild(itemSpanImg);
+    itemWrapATag.appendChild(itemInfo);
+
+    itemWrap.appendChild(itemWrapATag);
+
+    renderProduct.appendChild(itemWrap);
+  });
+}
+
+function eraseProducts(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
